62d348da4dac1d8148c48aa9b5c4643511725bfc
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-11-27 19:46:01 +0100

    Add generate Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import HomePage from "./Components/HomePage/Homepage"
 import Workshop from "./Components/Workshop/Workshop"
+import Banner from "./Components/Banner/Banner"
 import Style from "./style"
 import {Route, Routes} from "react-router-dom"
 
@@ -12,6 +13,7 @@ function App() {
    <Routes>
     <Route path="/" element={<HomePage/>}/>
     <Route path="/workshops" element={<Workshop/>}/>
+    <Route path="/banner" element={<Banner/>}/>
    </Routes>
   </>
  )
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar";
+
+const Banner = () => {
+    return (
+        <>
+            <Navbar/>
+            <div className="banner">
+            <h1>ReactJS</h1>
+            <p>Library for building user interfaces</p>
+            </div>
+        </>
+    );
+}
+
+export default Banner;
