f1cb5296bc8276a4b6cf3e7dfe4969d4b0e636a5
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-11-28 01:46:21 +0100

    Complete generate custom banner

--- src/Components/GetBanner.jsx
+++ src/Components/GetBanner.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const GetBanner = () => {
     return (
-        <div className="get-banner">
-            <h1>ReactJS</h1>
-            <p>Library for building user interfaces</p>
+        <div className="generate-banner">
+            <h1 sty>Generate Custom Banner</h1>
+            <p>Let people know you would be attending the event by generating a custom banner for yourself</p>
             <Link to='/banner'>Generate Banner</Link>
         </div>
     );
