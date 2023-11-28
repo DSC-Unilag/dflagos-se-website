59b22ca45596edd8f078afd746ae74274bdd6543
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-11-29 00:26:56 +0100

    Upload only square image

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
     const [uploadedImage, setUploadedImage] = useState(null);
     const [name, setName] = useState("");
     const [generating, setGenerating] = useState(false);
-    const [generated, setGenerated] = useState(false);
+    
 
     const handleNameChange = (event) => {
         setName(event.target.value);
@@ -18,18 +18,76 @@ const Banner = () => {
         document.querySelector('input[type="file"]').click();
     }
 
+    // const handleImageUpload = (e) => {
+    //     const file = e.target.files[0];
+    //     const reader = new FileReader();
+    //     getAspectRatio(file)
+    
+    //     reader.onload = () => {
+    //         setUploadedImage(reader.result);
+    //     };
+    
+    //     if (file) {
+    //         reader.readAsDataURL(file);
+    //     }
+    // };
+
     const handleImageUpload = (e) => {
         const file = e.target.files[0];
-        const reader = new FileReader();
+        
+        // Function to get the aspect ratio of an image
+        const getAspectRatio = (image) => {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.src = URL.createObjectURL(image);
+                
+                img.onload = () => {
+                    const width = img.width;
+                    const height = img.height;
+                    let aspectRatio;
+                    if (width < height) {
+                        aspectRatio = width / height;
+                    } else {
+                        aspectRatio = height / width;
+                    }
+                    console.log(width, height, aspectRatio)
+                    resolve(aspectRatio);
+                };
     
-        reader.onload = () => {
-            setUploadedImage(reader.result);
+                img.onerror = (error) => {
+                    reject(error);
+                };
+            });
         };
     
         if (file) {
-            reader.readAsDataURL(file);
+            // Check the aspect ratio
+            getAspectRatio(file)
+                .then((aspectRatio) => {
+                    // Define a threshold for an almost square image (e.g., 0.95)
+                    const threshold = 0.95;
+    
+                    if (aspectRatio > threshold) {
+                        // The image is a square or almost a square
+                        const reader = new FileReader();
+                        
+                        reader.onload = () => {
+                            setUploadedImage(reader.result);
+                        };
+                        
+                        reader.readAsDataURL(file);
+                    } else {
+                        // Display an error or take appropriate action for non-square images
+                        console.log('Please upload a square or almost square image.');
+                    }
+                })
+                .catch((error) => {
+                    // Handle errors during aspect ratio calculation
+                    console.error('Error getting aspect ratio:', error);
+                });
         }
     };
+    
     
     const handleDownload = (e) => {
         e.preventDefault();
@@ -48,18 +106,15 @@ const Banner = () => {
         });
 
         setGenerating(false)
-        setGenerated(true)
     };
 
     return (
-        <div style={{maxWidth: '980px', margin: '0 auto'}}>
+        <div style={{maxWidth: '980px', margin: '0 auto', padding: '0px 15px'}}>
             <Navbar/>
+            <hr />
 
-            
-
-
-            <div className="font-sans italic xl:max-w-[1280px] w-full get-banner">
-                <h1>Generate custom banner</h1>
+            <div className="get-banner">
+                <h1 style={{ fontWeight: 'bold', lineHeight: '110%', marginBottom: '20px' }}>Generate custom banner</h1>
                 <p>Let people know you would be attending the event by generating a custom banner for yourself</p>
 
                 <div className="banner-container">
@@ -83,9 +138,10 @@ const Banner = () => {
                             />
                         ) : (
                         <div onClick={handleSelectImage} className="choose-image">
-                            <p>Click to choose an image</p>
+                            <p>Click to choose a square image</p>
                         </div>
                         )}
+                        <p>Please upload only a square image</p>
 
                         <button>{generating ? "Downloading" : "Download"}</button>
                     </form>
@@ -127,7 +183,6 @@ const Banner = () => {
                 </div>
             </div>
             </div>
-            
             </div>
         </div>
     );
--- src/Components/GetBanner.jsx
+++ src/Components/GetBanner.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const GetBanner = () => {
     return (
         <div className="generate-banner">
-            <h1 sty>Generate Custom Banner</h1>
+            <h1 style={{ lineHeight: '110%' }}>Generate Custom Banner</h1>
             <p>Let people know you would be attending the event by generating a custom banner for yourself</p>
             <Link to='/banner'>Generate Banner</Link>
         </div>
