37a5e2bf771c157d253bb003698c033ba8d1cd0c
LordCodex <adenirandaniel565@gmail.com>
2023-11-30 15:58:10 +0100

    added a progress bar

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -46,37 +46,20 @@ const About = () => {
           Event
         </h1>
 
-     <div className='flex flex-col w-full lg:h-[50vh]'>
-       <h1 className='lg:max-w-[253px] text-[40px] lg:text-[94px] leading-none tracking-normal' style={{fontWeight: 700}}>About <br/>Event</h1>
-     
-     <div className='w-full flex mt-[24px] lg:pl-[305px]'>
-        <div className='border-y-2 border-[#4285F4] w-[100%]'>
-           <div className='flex flex-col sm:flex-row'>
-             <motion.p 
-             className='lg:max-w-[402px] pr-[14px] py-[19px]'
-             initial={{ opacity: 0, y: -150 }}
-             animate={controls}
-             transition={{ duration: 0.5}}
-             >
-             DevFestStudent’s Edition 2023 - is the first edition held specially 
-             for students with the aim of bringing technology closer to Students 
-             and tech enthusiasts passionate about technology.
-             </motion.p>
-            <div className='hidden sm:block border-l-2 border-[#4285F4] '/>
+        <div className="w-full flex mt-[24px] lg:pl-[305px]">
+          <div className="border-y-2 border-[#4285F4] w-[100%]">
+            <div className="flex flex-col sm:flex-row">
+              <p className="lg:max-w-[402px] pr-[14px] py-[19px] text-[20px] tracking-[-1.5px]">
+          DevFest Lagos Student Edition 2023 marks the inaugural event tailored specifically for students, aiming to bridge the gap between technology and the student community.
+              </p>
+              <div className="hidden sm:block border-l-2 border-[#4285F4] " />
 
-              <motion.p 
-              className='lg:max-w-[434px] py-[19px] sm:pl-[37px]'
-              initial={{ opacity: 0, y: -150 }}
-              animate={controls}
-              transition={{ duration: 0.5}}
-              >
-              Held in over 20 countries around the world, this is not just an event but a PLATFORM 
-              connecting Students with expert developers, and Tech enthusiasts 
-              </motion.p>
-           </div>
+              <p className="lg:max-w-[434px] py-[19px] sm:pl-[37px] text-[20px] tracking-[-1.5px]">
+              Explore developer tools, learn from experts, and connect with other developers from your local community.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
       </div>
     </section>
   );
