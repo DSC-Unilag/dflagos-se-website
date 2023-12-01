a555b74778c7b202561fc63307588b212f077389
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-12-01 01:59:55 +0100

    Complete assigned tasks - Delight

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import styles from "../style"
-import { Github, Microsoft, Google, AltSchool, Paystack, ArrowRight2, Mtn} from '../assets'
+import { Google} from '../assets'
 import newwave from '../assets/newwave.webp';
 import requestMechanic from '../assets/Request_mechanic.jpeg';
 import myTherapist from '../assets/Mytherapist.ng_logo_dark.png';
+import Marquee from "react-fast-marquee";
 
 const Clients = () => {
   return (
    <section id='clients' className={`text-white my-[20px] sm:my-[30px] lg:mt-[55px] bg-[#fff]`}>
     <div className='w-full grid grid-col-1 sm:grid-cols-3 lg:grid-cols-4 gap-[40px] sm:gap-[20px] place-content-center place-items-center py-[40px] xs:py-0'>
+
 
      <div>
         <a href="https://google.com" target="_blank">
