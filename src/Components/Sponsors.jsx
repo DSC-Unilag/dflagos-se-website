bba6e27835b0bc08083a39aef1b735aaee337734
LordCodex <adenirandaniel565@gmail.com>
2023-12-02 17:13:06 +0100

    implement animationincrease speed reduced the threshold disable animation on mobile

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -5,28 +5,16 @@ import newwave from '../assets/newwave.webp';
 import requestMechanic from '../assets/Request_mechanic.jpeg';
 import myTherapist from '../assets/Mytherapist.ng_logo_dark.png';
 import Marquee from "react-fast-marquee";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from 'react-intersection-observer';
+
 
 const Clients = () => {
-  const [ref, inView] = useInView({threshold: 0.8})
-  const controls = useAnimation()
-  const isMobile = window.innerWidth <= 768;
-  React.useEffect(() => {
-    if(inView && !isMobile){
-       controls.start({rotate: 0})
-    }
-  },[inView])
+
   return (
    <section id='clients' className={`text-white my-[20px] sm:my-[30px] lg:mt-[55px] bg-[#fff]`}>
     <div className='w-full grid grid-col-1 sm:grid-cols-3 lg:grid-cols-4 gap-[40px] sm:gap-[20px] place-content-center place-items-center py-[40px] xs:py-0'>
 
 
      <motion.div
-     ref={ref}
-     initial={!isMobile && {rotate: 50}}
-     animate={controls}
-     transition={{duration: 1}}
      >
         <a href="https://google.com" target="_blank">
           <img src={Google} alt='Google'/>
@@ -34,31 +22,19 @@ const Clients = () => {
       </motion.div>
 
       <motion.div
-      ref={ref}
-      initial={!isMobile && {rotate: -60}}
-      animate={controls}
-      transition={{duration: 1}}
       >
         <a href="https://newwave.io" target="_blank">
           <img src={newwave} alt='Google'/>
         </a>
       </motion.div>
 
-      <motion.div
-      ref={ref}
-      initial={!isMobile && {rotate: 50}}
-      animate={controls}
-      transition={{duration: 2}}>
+      <motion.div>
         <a href="https://www.mytherapist.ng" target="_blank">
           <img src={myTherapist} alt='Google'/>
         </a>
       </motion.div>
 
-      <motion.div
-      ref={ref}
-      initial={!isMobile && {rotate: -90}}
-      animate={controls}
-      transition={{duration: 2}}>
+      <motion.div>
         <a href="https://requestmechanic.com" target="_blank">
           <img src={requestMechanic} alt='Google'/>
         </a>
